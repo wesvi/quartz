119caa818144726c14c720ffa3c7ab598d48ba5e
wesvi <36145051+wesvi@users.noreply.github.com>
2024-10-06 00:00:55 +0800

    quart.layout.tsx

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/wesvi/quartz",
-      RSS: "https://note.wesvi.com/index.xml"
+      RSS: "https://note.wesvi.com/index.xml",
     },
   }),
 }
